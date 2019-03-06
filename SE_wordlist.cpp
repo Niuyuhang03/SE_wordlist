5d65ddf6a28c9103dcec6e15a78dd48bb42769f8
Niuyuhang03 <niuyuhang03@qq.com>
2019-03-07 02:30:51 +0800

    use Core class as encapsulation

--- SE_wordlist.cpp
+++ SE_wordlist.cpp
@@ -28,13 +28,21 @@ node::node(string cur_word, int cur_word_num, int cur_character_num) {
 	character_num = cur_character_num;
 }
 
+class Core {
+public:
+	int gen_chain_word(char* words[], int len, char* result[], char head, char tail, bool enable_loop);
+	int gen_chain_char(char* words[], int len, char* result[], char head, char tail, bool enable_loop);
+	bool gen_tree(node* cur_node, char* words[], int len, bool enable_loop, char tail, node* word_max_node, node* char_max_node);
+	bool find_in_chain(node* cur_node, string lower_word);
+};
+
 /*
 * 在cur_node所在链上找是否出现过lower_word
 * param:cur_node：当前节点
 * param:lower_word：要查找的单词
 * return:lower_word出现在cur_node所在链上返回true，否则返回false
 */
-bool find_in_chain(node* cur_node, string lower_word) {
+bool Core::find_in_chain(node* cur_node, string lower_word) {
 	string temp_word;
 	int i = 0;
 	node *child_node = cur_node->first_child, *parent_node = cur_node;
@@ -76,7 +84,7 @@ bool find_in_chain(node* cur_node, string lower_word) {
 * param:char_max_node：字母最多节点指针
 * return：出现环标志，enable_loop为false且出现环则返回true，不再生成树。否则返回false
 */
-bool gen_tree(node* cur_node, char* words[], int len, bool enable_loop, char tail, node* word_max_node, node* char_max_node) {
+bool Core::gen_tree(node* cur_node, char* words[], int len, bool enable_loop, char tail, node* word_max_node, node* char_max_node) {
 	node *temp_root;
 	char cur_tail = (char)tolower(cur_node->word[cur_node->word.size() - 1]);
 	int j;
@@ -143,7 +151,7 @@ bool gen_tree(node* cur_node, char* words[], int len, bool enable_loop, char tai
 * param:enable_loop：是否允许出现环标志，不允许成环时为false，否则为true
 * return：若没有链或enable_loop为false时出现环，返回0。否则返回环长度
 */
-int gen_chain_word(char* words[], int len, char* result[], char head, char tail, bool enable_loop) {
+int Core::gen_chain_word(char* words[], int len, char* result[], char head, char tail, bool enable_loop) {
 	int max_length = 0, cur_length = 0, i, root_node_cnt = 0;
 	node *root_node = NULL, *word_max_node, *char_max_node;
 	node* root_node_list[10000];
@@ -186,7 +194,7 @@ int gen_chain_word(char* words[], int len, char* result[], char head, char tail,
 * param:enable_loop：是否允许出现环标志，不允许成环时为false，否则为true
 * return：若没有链或enable_loop为false时出现环，返回0。否则返回环长度
 */
-int gen_chain_char(char* words[], int len, char* result[], char head, char tail, bool enable_loop) {
+int Core::gen_chain_char(char* words[], int len, char* result[], char head, char tail, bool enable_loop) {
 	int max_length = 0, cur_length = 0, i, root_node_cnt = 0;
 	node *root_node = NULL, *word_max_node, *char_max_node;
 	node* root_node_list[10000];
@@ -354,18 +362,19 @@ bool command_handler(int argc, const char* argv[], char* words[], int &len, char
 int main()//int argc, char* argv[]
 {
 	bool valid_flag, enable_loop = false, w_para = false;
-	int argc = 4, len = 0, index, chain_length;
-	const char* argv[4] = {"SE_wordlist.exe", "-r", "-w", "file.txt"};
+	int argc = 5, len = 0, index, chain_length;
+	const char* argv[5] = {"SE_wordlist.exe", "-h", "a", "-w", "file.txt"};
 	char *result[10000], *words[10000];
 	char head = 0, tail = 0;
+	Core core;
 
 	valid_flag = command_handler(argc, argv, words, len, head, tail, enable_loop, w_para);
 	if (!valid_flag)
 		return 0;
 	if (w_para)
-		chain_length = gen_chain_word(words, len, result, head, tail, enable_loop);
+		chain_length = core.gen_chain_word(words, len, result, head, tail, enable_loop);
 	else
-		chain_length = gen_chain_char(words, len, result, head, tail, enable_loop);
+		chain_length = core.gen_chain_char(words, len, result, head, tail, enable_loop);
 	if (chain_length < 2 && chain_length > 0) {
 		cout << "no chain's length is greater than 1!\n";
 		return 0;
